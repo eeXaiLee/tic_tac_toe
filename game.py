eb741117cd7aa241db433c7e0d463454cb23e012
Evgeniy Dimitriev <157293164+eeXaiLee@users.noreply.github.com>
2025-03-14 15:37:47 +0500

    Update game.py
    Add checking and handling exceptions

--- game.py
+++ game.py
@@ -1,16 +1,34 @@
 from gameparts import Board
+from gameparts.exceptions import FieldIndexError
 
 
 def main():
     game = Board()
-
     game.display()  # Отрисовать поле в терминале.
 
-    row = int(input('Введите номер строки: '))
-    column = int(input('Введите номер столбца: '))
+    while True:
+        try:
+            row = int(input('Введите номер строки: '))
+            if row < 0 or row >= game.field_size:
+                raise FieldIndexError
+
+            column = int(input('Введите номер столбца: '))
+            if column < 0 or column >= game.field_size:
+                raise FieldIndexError
+        except FieldIndexError:
+            print(
+                'Значение должно быть неотрицательным и меньше '
+                f'{game.field_size}.'
+                )
+            print(
+                'Пожалуйста, введите значения для строки и столбца заново.'
+                )
+            continue
+        else:
+            break
+
     game.make_move(row, column, 'X')
     print('Ход сделан!')
-    
     game.display()
 
 
